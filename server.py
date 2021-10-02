b144e66d095faab1ee0bf9eea41c4413bd9dd715
pavelino-is-learning <pavel_emanuel_ioan@yahoo.com>
2021-10-02 20:00:08 +0000

    Made the function __main__

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import socket
 
-def main():
+
+if __name__ == "__main__":
 	# next create a socket object
 	s = socket.socket()
 	print ("Socket successfully created")
-
 	# reserve a port on your computer in our
 	# case it is 12345 but it can be anything
 	port = 8080
-
 	# Next bind to the port
 	# we have not typed any ip in the ip field
 	# instead we have inputted an empty string
@@ -23,13 +22,13 @@ def main():
 
 	# a forever loop until we interrupt it or
 	# an error occurs
+
 	while True:
 		# Establish connection with client.
 		c, addr = s.accept()
 		print ('Got connection from', addr )
 		# send a thank you message to the client. encoding to send byte type.
 		c.send('Thank you for connecting'.encode())
-
 		# Close the connection with the client
 		c.close()
 		# Breaking once connection closed
