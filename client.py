6f262ce6eda8e33b6489080732d791c859315f80
Mos-Daniel-Ciprian <mos.dani@yahoo.com>
2021-10-03 08:30:12 +0000

    DDoS attack test

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import socket
 def main():
     s = socket.socket()
     port = 8080
-    s.connect(('10.0.0.2', port))
-    print (s.recv(1024).decode())
-    s.close()
+    while true:
+        s.connect(('10.0.0.2', port))
+        print (s.recv(1024).decode())
+        s.close()
 
 if __name__ == "main":
     main()
