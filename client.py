a8dfc56a5a94c74e9f31e1b6a2f33fc198684a6f
Mos-Daniel-Ciprian <mos.dani@yahoo.com>
2021-10-03 14:40:07 +0000

    Code change: socket declaration and port in loop

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ import socket
 import time
 
 def main():
-    s = socket.socket()
-    port = 8080
     while true:
+        s = socket.socket()
+        port = 8080
         s.connect(('10.0.0.9', port))
         print (s.recv(1024).decode())
         s.close()
