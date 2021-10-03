21c7b669dbfdae57fb45333cf515f8da8d5702b6
Mos-Daniel-Ciprian <mos.dani@yahoo.com>
2021-10-03 15:21:00 +0000

    Update client.py

--- client.py
+++ client.py
@@ -5,11 +5,13 @@ def main():
     while True:
         s = socket.socket()
         port = 8080
-        s.connect(('10.0.0.9', port))
-        string = s.recv(1024).decode()
-        if string:
-            print (string)
-        s.close()
+        try:
+            s.connect(('10.0.0.9', port))
+            string = s.recv(1024).decode()
+            if string:
+                print (string)
+        except:
+            s.close()
         time.sleep(0.75)
 
 if __name__ == "__main__":
