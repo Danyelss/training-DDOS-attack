d7cb1c73ac814bb6b8fa2cea6bd197a028240d52
Mos-Daniel-Ciprian <mos.dani@yahoo.com>
2021-10-03 14:09:20 +0000

    Refusing timestamp message added

--- server2.py
+++ server2.py
@@ -16,12 +16,11 @@ class ThreadedServer(object):
         while True:
             client, address = self.sock.accept()
             if address[0] not in addrs_dict.keys() or addrs_dict[address[0]] <= time.time():
-                print(addrs_dict)
                 addrs_dict[address[0]] = time.time() + self.timer
                 threading.Thread(target = self.listenToClient,args = (client,address)).start()
             else:
                 client.close()
-                print("You shall not pass !!!")
+                print("Refusing to answer to " + str(address[0]) + " because it made a request " + str(self.timer - addrs_dict[address[0]] + time.time()) + " seconds ago" )
 
     def listenToClient(self, client, address):
         print ('Connected to ', address )
